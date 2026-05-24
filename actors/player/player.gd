836c63a5c12a203fa246c8df68c64b86902e351c
aXu-AP <1621768+aXu-AP@users.noreply.github.com>
2026-05-24 22:36:14 +0300

    Improve game feel

--- actors/player/player.gd
+++ actors/player/player.gd
@@ -1,18 +1,26 @@
 extends CharacterBody3D
 
-@export var speed: float = 15.0
+@export var forward_speed: float = 15.0
+@export var boosting_speed: float = 20.0
+@export var braking_speed: float = 10.0
+@export var vertical_speed: float = 15.0
 @export var turning_speed: float = 2.0
 @export var max_pitch: float = PI / 2 * .8
 @export var max_height: float = 10.0
 @export var max_height_soft: float = 7.0
 @export var downwards_force: float = 30.0
 
+
 var _turning_speed: float = 0.0
-@onready var _speed: float = speed
+@onready var _speed: float = forward_speed
 
 func _physics_process(delta: float) -> void:
-	_speed = move_toward(_speed, speed - rotation.x * 5, delta * 10)
-	velocity = -basis.z * _speed
+	var base_speed: float = forward_speed
+	base_speed = lerp(base_speed, boosting_speed, Input.get_action_strength("boost"))
+	base_speed = lerp(base_speed, braking_speed, Input.get_action_strength("brake"))
+	_speed = move_toward(_speed, base_speed, delta * (10 + Input.get_action_strength("boost") * 10))
+	velocity = Vector3.FORWARD.rotated(Vector3.UP, rotation.y) * _speed
+	velocity.y = rotation.x * vertical_speed
 	var target_rotation = Input.get_axis("turn_right", "turn_left") * turning_speed
 	_turning_speed = move_toward(_turning_speed, target_rotation, 10 * delta)
 	rotate_y(_turning_speed * delta)
--- actors/player/player_camera.gd
+++ actors/player/player_camera.gd
@@ -1,12 +1,13 @@
 extends Camera3D
 
 @export var target: Node3D
-@export var distance: float = 8.0
+@export var distance: float = 7.0
+@export var height: float = 8.0
 
 func _physics_process(delta: float) -> void:
 	var target_flat: Vector3 = target.global_position - target.global_basis.z * 5
-	target_flat.y = global_position.y
+	target_flat.y = global_position.y - 3
 	look_at(target_flat)
 	var target_position = target.global_position + Vector3.BACK.rotated(Vector3.UP, target.rotation.y) * distance
-	global_position = global_position.lerp(target_position, .05)
-	global_position.y = lerp(global_position.y, 5.0, .2)
+	global_position = global_position.lerp(target_position, .04)
+	global_position.y = lerp(global_position.y, height, .2)
