d498710fb5bae0ca43be479513cfd360e2048adb
aXu-AP <1621768+aXu-AP@users.noreply.github.com>
2026-06-06 17:10:16 +0300

    Revamp quest system
    Make Quest class more abstract and flexible.
    Add simple cutscene functionality.
    Move box functionality into ParcelQuest.

--- actors/quest_point/quest_point.gd
+++ actors/quest_point/quest_point.gd
@@ -1,51 +1,57 @@
 class_name QuestPoint
 extends Area3D
 
-## Quest that starts when touching this quest point.
-@export var quest_to_start: Quest
-## Quest that this quest point belongs to.
-## If not empty, the quest point will be disabled until the quest has started.
-@export var quest_to_goal: Quest
+signal quest_started
+signal quest_ended(end_state: Quest.EndState)
+
+## What to do with the quest. Possible values: [code]start[/code], [code]goal[/code] or custom-defined ones.
+@export var action: Quest.Action
+## Set of flags that are required for this questpoint to show up.
+## Additionally the visibility is controlled by quest status.
+@export var required_flags: Array[StringName] = []
+## How many seconds to wait after required flags are available until the questpoint will show up.
+@export var availability_delay: int = 0
+## Quest that the action gets sent on interact.
+@export var quest: Quest
 
 
 func _ready() -> void:
-	if quest_to_start != null:
-		if quest_to_start.name in Globals.flags:
-			queue_free()
-		quest_to_start.ended.connect(on_quest_to_start_ended.unbind(1))
-	if quest_to_goal != null:
-		quest_to_goal.started.connect(on_quest_started)
-		quest_to_goal.ended.connect(on_quest_to_goal_ended.unbind(1))
-		if quest_to_goal.status != Quest.Status.ACTIVE:
-			visible = false
+	update_visibility()
+	quest.status_changed.connect(update_visibility.unbind(1))
+	Globals.flag_added.connect(update_visibility.bind(availability_delay).unbind(1))
+	quest.started.connect(quest_started.emit)
+	quest.ended.connect(quest_ended.emit)
 
 
-func on_quest_started() -> void:
-	visible = true
+func update_visibility(delay: int = 0) -> void:
+	if delay:
+		await get_tree().create_timer(delay).finished
+	# The quest is completed, quest no longer available.
+	if quest.name in required_flags:
+		queue_free()
+	visible = check_visibility()
+	set_deferred("monitoring", visible)
 
 
-func on_quest_to_start_ended() -> void:
-	visible = true
-
-
-func on_quest_to_goal_ended() -> void:
-	visible = false
+func check_visibility() -> bool:
+	if not required_flags.all(func (f): return f in Globals.flags):
+		return false
+	if action == Quest.Action.START and quest.status != Quest.Status.AVAILABLE:
+		return false
+	if action != Quest.Action.START and quest.status != Quest.Status.ACTIVE:
+		return false
+	return true
 
 
 func interact() -> void:
 	if not visible:
 		return
-	if quest_to_goal != null:
-		quest_to_goal.goals_left -= 1
-		if quest_to_goal.attach_boxes:
-			Box.collect(global_position)
-		if quest_to_goal.goals_left == 0:
-			quest_to_goal.end_quest(Quest.EndState.COMPLETE)
-	if quest_to_start != null and quest_to_start.start_quest():
-		if quest_to_start.attach_boxes:
-			for i in quest_to_start.goals:
-				var box = preload("uid://7l20j2o7xj5i").instantiate()
-				box.quest = quest_to_start
-				get_parent().add_child(box)
-				box.global_position = global_position
+	quest.do_action(self, action)
 	visible = false
+	set_deferred("monitoring", false)
+	var anim := get_node_or_null("AnimationPlayer") as AnimationPlayer
+	if anim and anim.has_animation(&"cutscene"):
+		anim.process_mode = Node.PROCESS_MODE_ALWAYS
+		anim.play(&"cutscene", 1)
+		get_tree().paused = true
+		anim.animation_finished.connect(get_tree().set.bind("paused", false).unbind(1))
---
+++ misc/quests/quest.gd
@@ -0,0 +1,113 @@
+class_name Quest
+extends Resource
+
+signal started
+signal ended(end_state: EndState)
+signal status_changed(new_status: Status)
+
+enum Status {
+	LOCKED,
+	AVAILABLE,
+	ACTIVE,
+	COMPLETED,
+}
+
+enum EndState {
+	COMPLETE,
+	TIMEOUT,
+	BROKEN,
+	CRASH,
+	FAIL_TRIGGER,
+	ABANDON,
+	CONTINUE,
+	CUSTOM1,
+	CUSTOM2,
+	CUSTOM3,
+}
+
+enum Action {
+	START,
+	GOAL,
+	FAIL,
+	CUSTOM1,
+	CUSTOM2,
+	CUSTOM3,
+}
+
+static var active_quest: Quest
+
+@export var name: String
+@export var goals: int = 1
+@export var time_limit: int = 0
+@export var status := Status.AVAILABLE:
+	get():
+		return status
+	set(val):
+		status = val
+		status_changed.emit(status)
+@export var start_message: Message
+@export var end_messages: Dictionary[EndState, Message]
+@export var mid_goal_messages: Array[Message]
+
+
+var goals_left: int
+var timer: SceneTreeTimer
+
+
+## Responds to given action.
+## Override to react to custom actions.
+func do_action(source: Node3D, action: Action) -> void:
+	match action:
+		Action.START: start_quest(source)
+		Action.GOAL: end_quest(try_goal(source))
+		Action.FAIL: end_quest(EndState.FAIL_TRIGGER)
+
+
+## Marks a subgoal completed and checks if the quest can be completed.
+## Can be overridden for custom condition checking.
+func try_goal(_source: Node3D) -> EndState:
+	goals_left -= 1
+	if goals_left <= 0:
+		return EndState.COMPLETE
+	return EndState.CONTINUE
+
+
+## Starts the quest. If another quest is ongoing, abandon it.
+func start_quest(_source: Node3D) -> bool:
+	if status != Status.AVAILABLE:
+		return false
+	
+	if active_quest:
+		active_quest.end_quest(EndState.ABANDON)
+	
+	print("%s started" % name)
+	status = Quest.Status.ACTIVE
+	active_quest = self
+	goals_left = goals
+	started.emit()
+	if start_message:
+		DialogueBox.queue_message(start_message)
+	if time_limit > 0:
+		timer = Player.instance.get_tree().create_timer(time_limit, false)
+		timer.timeout.connect(end_quest.bind(EndState.TIMEOUT))
+	return true
+
+
+## Ends the quest unless state is CONTINUE. Displays a corresponding message from end_messages.
+func end_quest(state: EndState) -> bool:
+	if status != Quest.Status.ACTIVE or state == EndState.CONTINUE:
+		return false
+	if is_instance_valid(timer) and timer.timeout.is_connected(end_quest.bind(EndState.TIMEOUT)):
+		timer.timeout.disconnect(end_quest.bind(EndState.TIMEOUT))
+	print("%s ended with %s" % [name, state])
+	if state == EndState.COMPLETE:
+		status = Status.COMPLETED
+		Globals.add_flag(name)
+	else:
+		status = Status.AVAILABLE
+	var message = end_messages.get(state)
+	if message:
+		DialogueBox.queue_message(message)
+	ended.emit(state)
+	active_quest = null
+	return true
