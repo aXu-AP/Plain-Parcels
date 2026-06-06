fa26f949809e05c3a4e1fb86a96b0f8a2cbdee65
aXu-AP <1621768+aXu-AP@users.noreply.github.com>
2026-06-06 19:30:55 +0300

    Add Coin Quest

--- actors/coin/coin.gd
+++ actors/coin/coin.gd
@@ -1,9 +1,7 @@
 extends Area3D
 
 
-func enable() -> void:
-	set_deferred("monitoring", true)
-	$MeshInstance3D.visible = true
+@export var is_quest_coin := false
 
 
 func _process(delta: float) -> void:
@@ -11,7 +9,22 @@ func _process(delta: float) -> void:
 
 
 func _on_area_entered(area: Area3D) -> void:
-	Globals.coins += 1
+	if is_quest_coin:
+		Globals.quest_coins += 1
+	else:
+		Globals.coins += 1
 	$MeshInstance3D.visible = false
 	$CollectParticles.emitting = true
 	set_deferred("monitoring", false)
+
+
+func appear() -> void:
+	set_deferred("monitoring", true)
+	$MeshInstance3D.visible = true
+	set_process(true)
+
+
+func disappear() -> void:
+	set_deferred("monitoring", false)
+	$MeshInstance3D.visible = false
+	set_process(false)
